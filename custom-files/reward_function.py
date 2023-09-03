4a96d3a54d61e0a3a57a3e63c6f9f657942d53d4
nvbushra <101579057+nvbushra@users.noreply.github.com>
2023-09-03 16:52:05 -0400

    Update reward_function.py

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,9 +1,19 @@
 def reward_function(params):
-    '''
-    Example of rewarding the agent to follow center line
-    '''
-    
+
     # Read input parameters
     speed = params['speed']
+    progress = params['progress']
+    steps = params['steps']
+    all_wheels_on_track = params['all_wheels_on_track']
     
-    return float(speed)
+    reward = 0.0001
+    TOTAL_NUM_STEPS = 250
+    
+    if all_wheels_on_track:
+        reward = speed
+        if(progress == 100) and steps <= TOTAL_NUM_STEPS:
+            reward += 100 + speed**2
+        elif progress > (steps / TOTAL_NUM_STEPS) * 100:
+            reward = reward + speed**2 
+        
+    return float(reward)
